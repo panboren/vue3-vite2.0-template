de4da49615d51cb9415a7eb2bb71e241053642c1
panboren <3248874747@qq.com>
2022-03-09 15:51:49 +0800

    添加element-plush

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './assets/css/reset.min.css'
 import store from "./store";
 import router from "./router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
